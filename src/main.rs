e7c4a2b5830b23500ef58cfd5adf094fb7c3b04a
Conley Laptop <conleygibbs@yahoo.com>
2021-05-10 19:56:25 -0500

    Reads in a file and can print lines matching pattern

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,21 @@ struct Cli {
     /// The pattern to look for
     pattern: String,
     // Pathbuf is like a string but for file system paths that is cross platform
+    #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
 fn main() {
     let args = Cli::from_args();
 
-    let pattern = std::env::args().nth(1).expect("No pattern given");
-    let path = std::env::args().nth(2).expect("No path given");
+    //The expect function will cause the program to exit if the value could not be read
+    let content = std::fs::read_to_string(&args.path)
+        .expect("Could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
 }
