b8e2dda735c2e5617da7669f342bddb6d5a90c20
Conley Laptop <conleygibbs@yahoo.com>
2021-05-10 19:49:38 -0500

    Added parsing cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use structopt::StructOpt;
 
-struct CLI
-{
+/// Search for a pattern in a file and display the lines that contain it
+#[derive(StructOpt)]
+struct Cli {
+    /// The pattern to look for
     pattern: String,
     // Pathbuf is like a string but for file system paths that is cross platform
     path: std::path::PathBuf,
 }
 
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
 
     let pattern = std::env::args().nth(1).expect("No pattern given");
     let path = std::env::args().nth(2).expect("No path given");
