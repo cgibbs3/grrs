0bce717a544036edfc018ab5e018cb049b5f22ad
Conley Laptop <conleygibbs@yahoo.com>
2021-05-11 19:46:04 -0500

    Setting up buffered reader

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use structopt::StructOpt;
+use std::io::{BufRead, BufReader};
 
 /// Search for a pattern in a file and display the lines that contain it
 #[derive(StructOpt)]
@@ -10,17 +11,29 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
-    let args = Cli::from_args();
+fn main() -> std::io::Result<()> {
+    //let args = Cli::from_args();
 
     //The expect function will cause the program to exit if the value could not be read
-    let content = std::fs::read_to_string(&args.path)
-        .expect("Could not read file");
+    //let content = std::fs::read_to_string(&args.path)
+    //   .expect("Could not read file");
 
+    let f = std::fs::File::open("C:\\Users\\Conley\\Code\\Rust\\grrs\\src\\main.rs")?;
+    let mut reader = BufReader::new(f);
+
+    let mut line = String::new();
+    let len = reader.read_line(&mut line)?;
+    println!("First line is {} bytes long", len);
+
+    //let mut line = String::new();
+    
+    //for line in f.
+    /*
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
-
+    */
+    Ok(())
 }
